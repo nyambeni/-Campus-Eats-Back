1aebe5bbae3ee5e279c0c09d7c88f655e822660e
mnisi-coder <59182644+mnisi-coder@users.noreply.github.com>
2021-01-26 13:34:47 +0200

    This is for database connection

---
+++ Config/conn.js
@@ -0,0 +1,27 @@
+//this is the only one needed here 
+var mysql = require('mysql');
+
+//npm isntall sql
+
+
+//connect to database
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'Kota'
+});
+ 
+ 
+connection.connect(function(err) {
+  if (err)
+  {
+	  throw err
+	  console.log('You are now connected with mysql database...')
+  }else{
+  console.log('You are now connected with mysql database...')
+  }
+})
+
+module.exports = connection;
+
